758d5dba61c6da3f4a402ec9fce0ef1c15041f8d
demaio89 <demaio89@gmail.com>
2021-08-12 08:38:50 -0500

    basic structure of quiz implemented

---
+++ script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'How old is Florin?',
+        a: '10',
+        b: '17',
+        c: '26',
+        d: '110',
+        correct: 'c' 
+    }, {
+        question: 'What is the most used programming language in 2019', 
+        a: 'Java',
+        b: 'C', 
+        c: 'Python',
+        d: 'Javascript',
+        correct: 'd'
+    }, {
+        question: 'Who is the President of US?', 
+        a: 'Florin Pop',
+        b: 'Donald Trump',
+        c: 'Ivan Saldano',
+        d: 'Biden',
+        correct: 'd'
+    }, {
+        question: 'What does HTML stand for?',
+        a: "Hypertext markup language", 
+        b: 'Cascading Style Sheet',
+        c: 'Json Object Notation',
+        d: 'Helicopters Terminals Motorboats Lamborginis',
+        correct: 'a'
+    }, {
+        question: 'What year was Javascript Launched?',
+        a: '1997',
+        b: '1996',
+        c: '1994', 
+        d: 'none of the above',
+        correct: 'd'
+    }
+]
